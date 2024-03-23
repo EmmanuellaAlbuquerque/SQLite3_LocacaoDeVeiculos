4bce3c52e4fd617723f7079f67dea655c39eaeec
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-23 19:18:19 -0300

    Criando a tabela CAR_BRAND e inserindo registros nela

---
+++ 04-CAR_BRAND/Inserindo dados na tabela CAR_BRAND.sql
@@ -0,0 +1,14 @@
+-- Tarefa 04 (23/03/2024)
+-- Inserindo dados na tabela CAR_BRAND (marcas)
+
+INSERT INTO CAR_BRAND (BRAND_NAME) 
+  VALUES ('Chevrolet'),
+         ('Toyota'),
+         ('Hyundai'),
+         ('Volkswagen'),
+         ('Jeep'),
+         ('Renault'),
+         ('Honda'),
+         ('Fiat');
+
+SELECT * FROM CAR_BRAND;
