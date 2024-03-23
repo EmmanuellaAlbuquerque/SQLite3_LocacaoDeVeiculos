f2e73370f365e2e8178c4034959b51fd63b67bab
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-23 19:06:13 -0300

    Criando a tabela CAR_MODEL e inserindo registros nela

---
+++ 03-CAR_MODEL/Inserindo dados na tabela CAR_MODEL.sql
@@ -0,0 +1,16 @@
+-- Tarefa 03 (23/03/2024)
+-- Inserindo dados na tabela CAR_MODEL (modelos)
+
+INSERT INTO CAR_MODEL (MODEL_NAME) 
+  VALUES ('Conversível'),
+         ('Sedã'),
+         ('Hatch'),
+         ('Coupé'),
+         ('Perua'),
+         ('SUV'),
+         ('Picape'),
+         ('Minivan'),
+         ('Utilitário'),
+         ('Buggy');
+
+SELECT * FROM CAR_MODEL;
