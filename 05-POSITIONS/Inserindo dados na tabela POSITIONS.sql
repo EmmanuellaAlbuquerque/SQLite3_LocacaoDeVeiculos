32039fc96949d3f3bf04b023a28c11898b75afd1
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-23 19:27:56 -0300

    Criando a tabela POSITIONS e inserindo registros nela

---
+++ 05-POSITIONS/Inserindo dados na tabela POSITIONS.sql
@@ -0,0 +1,11 @@
+-- Tarefa 05 (23/03/2024)
+-- Inserindo dados na tabela POSITIONS (Cargos)
+
+INSERT INTO POSITIONS (DESCRIPTION) 
+  VALUES ('Gerente de vendas'),
+         ('Gerente de compras'),
+         ('Vendedor'),
+         ('Mecânico'),
+         ('Assistente Administrativo');
+
+SELECT * FROM POSITIONS;
