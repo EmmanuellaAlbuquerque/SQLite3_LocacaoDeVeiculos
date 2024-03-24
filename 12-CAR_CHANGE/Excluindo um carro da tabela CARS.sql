1df829c43da65d098259595fd448fa9b887e56c8
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-24 12:15:33 -0300

    Excluindo um CAR

---
+++ 12-CAR_CHANGE/Excluindo um carro da tabela CARS.sql
@@ -0,0 +1,7 @@
+-- Tarefa 12 (24/03/2024)
+-- Excluindo um registro da tabela CARS (Carros)
+
+DELETE FROM CARS WHERE NAME = 'Hyundai HB20 1.6';
+
+SELECT * FROM CARS WHERE NAME = 'Hyundai HB20 1.6';
+SELECT * FROM CARS;
