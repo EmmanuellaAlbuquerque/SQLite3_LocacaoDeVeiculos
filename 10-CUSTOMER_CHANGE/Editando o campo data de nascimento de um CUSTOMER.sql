51a213bbfa8f5fc780587e464c8a18d1561a8c58
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-24 12:01:27 -0300

    Editando o campo data de nascimento de um CUSTOMER

---
+++ 10-CUSTOMER_CHANGE/Editando o campo data de nascimento de um CUSTOMER.sql
@@ -0,0 +1,7 @@
+-- Tarefa 010 (24/03/2024)
+-- Editando um campo de um registro da tabela CUSTOMERS (Clientes)
+
+UPDATE CUSTOMERS SET BIRTH_DATE = '1986-06-19' WHERE NAME = 'Josefa';
+
+SELECT * FROM CUSTOMERS WHERE NAME = 'Josefa';
+SELECT * FROM CUSTOMERS;
