a2c1875b134e1d02bfe1e76500fd08b999d524ad
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-24 18:18:12 -0300

    Calculando a quantidade de LOCATIONS realizadas

---
+++ 21-LOCATIONS_COUNT/Exibindo a quantidade de locações realizadas.sql
@@ -0,0 +1,6 @@
+-- Tarefa 21 (24/03/2024)
+-- Quantidade de locações realizadas na tabela LOCATIONS (Locações)
+
+SELECT 
+  COUNT(*) AS 'Quantidade TOTAL de locações realizadas'  
+  FROM LOCATIONS;
