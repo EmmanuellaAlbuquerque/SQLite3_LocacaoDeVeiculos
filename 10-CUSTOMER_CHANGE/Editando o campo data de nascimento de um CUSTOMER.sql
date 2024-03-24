513dd1662a4d0875263b731558406baf5d029cc2
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-24 12:09:09 -0300

    Editando o campo ano de um CAR

--- 10-CUSTOMER_CHANGE/Editando o campo data de nascimento de um CUSTOMER.sql
+++ 10-CUSTOMER_CHANGE/Editando o campo data de nascimento de um CUSTOMER.sql
@@ -1,4 +1,4 @@
--- Tarefa 010 (24/03/2024)
+-- Tarefa 10 (24/03/2024)
 -- Editando um campo de um registro da tabela CUSTOMERS (Clientes)
 
 UPDATE CUSTOMERS SET BIRTH_DATE = '1986-06-19' WHERE NAME = 'Josefa';
---
+++ 11-CAR_CHANGE/Editando o ano de um CAR.sql
@@ -0,0 +1,7 @@
+-- Tarefa 11 (24/03/2024)
+-- Editando um campo de um registro da tabela CARS (Carros)
+
+UPDATE CARS SET YEAR = 2019 WHERE NAME = 'Fiat Cronos';
+
+SELECT * FROM CARS WHERE NAME = 'Fiat Cronos';
+SELECT * FROM CARS;
